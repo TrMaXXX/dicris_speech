8aefa966896b8bb79ab3d67c44e358fe33edd490
marian <trnka@savba.sk>
2025-10-06 09:04:56 +0200

    -

--- pieces/RandomNumPiece/piece.py
+++ pieces/RandomNumPiece/piece.py
@@ -10,6 +10,14 @@ class RandomNumPiece(BasePiece):
         number=int(random.random()*100)
         # Set display result
         # Return output
+        raw_content = f"Random number is: {number}\n"
+        base64_content = base64.b64encode(raw_content.encode("utf-8")).decode("utf-8")
+        self.display_result = {
+            "file_type": "txt",
+            "base64_content": base64_content
+        }
+
+        self.logger.info(f"Random number is: {number}")
         return OutputModel(
             number=number
         )
